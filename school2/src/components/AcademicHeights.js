e250b13a377e005b34c654b2b1d191bbcc641e0d
Samrat0709 <118931358+Samrat0709@users.noreply.github.com>
2023-10-30 16:16:23 +0530

    Home page responsive

--- school2/src/components/AcademicHeights.js
+++ school2/src/components/AcademicHeights.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import person2 from '../img/person2.webp';
+import { FreeMode } from 'swiper/modules';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
 
 
 const AcademicHeights = () => {
@@ -8,42 +14,76 @@ const AcademicHeights = () => {
         <div className="academic">
             <h3>ACADEMIC HEIGHTS</h3>
             <div className="students">
-                <div className="student">
-                    <div className="img-container">
-                        <img src={person2} alt="" />
-                    </div>
-                    <div className="text">
-                        <h4>Kenny Omega </h4>
-                        <h5>99.96(CBSE 12th)</h5>
-                    </div>
-                </div>
-                <div className="student">
-                    <div className="img-container">
-                        <img src={person2} alt="" />
-                    </div>
-                    <div className="text">
-                        <h4>Kenny Omega </h4>
-                        <h5>99.96(CBSE 12th)</h5>
-                    </div>
-                </div>
-                <div className="student">
-                    <div className="img-container">
-                        <img src={person2} alt="" />
-                    </div>
-                    <div className="text">
-                        <h4>Kenny Omega </h4>
-                        <h5>99.96(CBSE 12th)</h5>
-                    </div>
-                </div>
-                <div className="student">
-                    <div className="img-container">
-                        <img src={person2} alt="" />
-                    </div>
-                    <div className="text">
-                        <h4>Kenny Omega </h4>
-                        <h5>99.96(CBSE 12th)</h5>
-                    </div>
-                </div>
+                <Swiper
+
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    freeMode={false}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                            spaceBetween: 50,
+                        },
+                    }}
+                    modules={[FreeMode]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <div className="student">
+                            <div className="img-container">
+                                <img src={person2} alt="" />
+                            </div>
+                            <div className="text">
+                                <h4>Kenny Omega </h4>
+                                <h5>99.96(CBSE 12th)</h5>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="student">
+                            <div className="img-container">
+                                <img src={person2} alt="" />
+                            </div>
+                            <div className="text">
+                                <h4>Kenny Omega </h4>
+                                <h5>99.96(CBSE 12th)</h5>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="student">
+                            <div className="img-container">
+                                <img src={person2} alt="" />
+                            </div>
+                            <div className="text">
+                                <h4>Kenny Omega </h4>
+                                <h5>99.96(CBSE 12th)</h5>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="student">
+                            <div className="img-container">
+                                <img src={person2} alt="" />
+                            </div>
+                            <div className="text">
+                                <h4>Kenny Omega </h4>
+                                <h5>99.96(CBSE 12th)</h5>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                </Swiper>
+
+                
             </div>
 
         </div>
--- school2/src/components/Talks.js
+++ school2/src/components/Talks.js
@@ -1,26 +1,111 @@
 import React from 'react'
 import person3 from '../img/person3.webp';
+import { FreeMode } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
 const Talks = () => {
     return (
         <div className="talks">
             <h2>
                 They talk about us
             </h2>
-            <div className="slide">
-                <div className="left">
-                    <div className="img-container">
-                        <img src={person3} alt="" />
+            <Swiper
+
+                slidesPerView={1}
+                spaceBetween={30}
+                freeMode={false}
+                // breakpoints={{
+                //     640: {
+                //         slidesPerView: 2,
+                //         spaceBetween: 20,
+                //     },
+                //     768: {
+                //         slidesPerView: 3,
+                //         spaceBetween: 40,
+                //     },
+                //     1024: {
+                //         slidesPerView: 4,
+                //         spaceBetween: 50,
+                //     },
+                // }}
+                modules={[FreeMode]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="left">
+                            <div className="img-container">
+                                <img src={person3} alt="" />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <h4>Does the school hold PTM on regular basis?</h4>
+                            <p>Yes, the school and parents together can only support the child to grow in a desired way. Hence it holds PTMs regularly and welcome parent's suggestions.</p>
+                            <div className="name">
+                                <h5>Subhranil Maity</h5>
+                                <h6>CEO oF CREASUNX</h6>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="right">
-                    <h4>Does the school hold PTM on regular basis?</h4>
-                    <p>Yes, the school and parents together can only support the child to grow in a desired way. Hence it holds PTMs regularly and welcome parent's suggestions.</p>
-                    <div className="name">
-                        <h5>Subhranil Maity</h5>
-                        <h6>CEO oF CREASUNX</h6>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="left">
+                            <div className="img-container">
+                                <img src={person3} alt="" />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <h4>Does the school hold PTM on regular basis?</h4>
+                            <p>Yes, the school and parents together can only support the child to grow in a desired way. Hence it holds PTMs regularly and welcome parent's suggestions.</p>
+                            <div className="name">
+                                <h5>Subhranil Maity</h5>
+                                <h6>CEO oF CREASUNX</h6>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="left">
+                            <div className="img-container">
+                                <img src={person3} alt="" />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <h4>Does the school hold PTM on regular basis?</h4>
+                            <p>Yes, the school and parents together can only support the child to grow in a desired way. Hence it holds PTMs regularly and welcome parent's suggestions.</p>
+                            <div className="name">
+                                <h5>Subhranil Maity</h5>
+                                <h6>CEO oF CREASUNX</h6>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="left">
+                            <div className="img-container">
+                                <img src={person3} alt="" />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <h4>Does the school hold PTM on regular basis?</h4>
+                            <p>Yes, the school and parents together can only support the child to grow in a desired way. Hence it holds PTMs regularly and welcome parent's suggestions.</p>
+                            <div className="name">
+                                <h5>Subhranil Maity</h5>
+                                <h6>CEO oF CREASUNX</h6>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+
         </div>
     )
 }
